8ca195efe7bc728f1eca64488b65f698989d109d
Ameliah Utary <ameliah1244@gmail.com>
2024-05-13 12:58:50 +0800

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.getElementById('search-icon').addEventListener('click', function() {
+    var searchBox = document.getElementById('search-box');
+    if (searchBox.style.display === 'none' || searchBox.style.display === '') {
+        searchBox.style.display = 'block';
+    } else {
+        searchBox.style.display = 'none';
+    }
+});
