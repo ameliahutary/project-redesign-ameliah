e0154d914ad03cd2476a945ef27e4647c9a0a5a6
Ameliah Utary <ameliah1244@gmail.com>
2024-05-13 13:12:10 +0800

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // script.js
 
-document.getElementById('search-btn').addEventListener('click', function() {
-    var menuSearch = document.getElementById('menu-search');
-    menuSearch.classList.toggle('active');
+document.addEventListener("DOMContentLoaded", function() {
+    var searchIcon = document.querySelector(".search-btn");
+    var searchBox = document.querySelector(".search-box");
+
+    searchIcon.addEventListener("click", function() {
+        if (searchBox.style.display === "none") {
+            searchBox.style.display = "block";
+        } else {
+            searchBox.style.display = "none";
+        }
+    });
 });
