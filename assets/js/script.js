850020d38aeba7cd365f6104589a07fdf04be68b
Ameliah Utary <ameliah1244@gmail.com>
2024-05-13 13:09:13 +0800

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 // script.js
 
-document.getElementById('search-icon').addEventListener('click', function() {
-    var searchBox = document.getElementById('search-box');
-    if (searchBox.style.display === 'none' || searchBox.style.display === '') {
-        searchBox.style.display = 'block';
-    } else {
-        searchBox.style.display = 'none';
-    }
+document.getElementById('search-btn').addEventListener('click', function() {
+    var menuSearch = document.getElementById('menu-search');
+    menuSearch.classList.toggle('active');
 });
